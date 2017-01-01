b6b09afbd111c1685d40cde36fb16ad80719ea81
Shangeri Sivalingam <shangeris.2013@sis.smu.edu.sg>
2017-01-01 22:26:49 +0800

    Created graph using Python
    Using Flask framework to display graph in HTML

--- Attempted Codes/Third trial/hello.py
+++ Attempted Codes/Third trial/hello.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, make_response
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+	
     return "Hello World!"
 
 if __name__ == "__main__":
