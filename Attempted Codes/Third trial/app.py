df00820ca89156f33333e9b690d344bcd929adae
Shangeri Sivalingam <shangeris.2013@sis.smu.edu.sg>
2017-01-02 23:49:34 +0800

    Modify Code to take in Data from File
    Application now reads data from file and display in graph form

--- Attempted Codes/Third trial/app.py
+++ Attempted Codes/Third trial/app.py
@@ -2,13 +2,25 @@ from flask import Flask
 from flask import Markup
 from flask import Flask
 from flask import render_template
+import csv
+
 app = Flask(__name__)
- 
+
+exampleFile = open('apple dec sample set.csv')
+exampleReader = csv.reader(exampleFile)
+#exampleData = list(exampleReader)
+
+labels = []
+values = []
+
+
+
 @app.route("/")
 def chart():
-    labels = ["January","February","March","April","May","June","July","August"]
-    values = [10,9,8,7,6,4,7,8]
-    return render_template('chart.html', values=values, labels=labels)
+	for row in exampleReader:
+		labels.append(row[0])
+		values.append(row[1])
+	return render_template('chart.html', values=values, labels=labels)
  
 if __name__ == "__main__":
     app.run()
