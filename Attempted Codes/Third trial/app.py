fedfc1e52e7fc45d97e83274d2c84a5b7e3d9618
Shangeri Sivalingam <shangeris.2013@sis.smu.edu.sg>
2017-01-04 23:15:58 +0800

    Attempt to plot 2 graph on one axis
    app.py does not plot 2 graphs. multiple_figs_demo.py plots 2 graphs but
    does not take in data from csv

--- Attempted Codes/Third trial/app.py
+++ Attempted Codes/Third trial/app.py
@@ -2,13 +2,26 @@ from flask import Flask
 from flask import Markup
 from flask import Flask
 from flask import render_template
+import csv
+
 app = Flask(__name__)
- 
+
+exampleFile = open('apple dec sample set.csv')
+exampleReader = csv.reader(exampleFile)
+#exampleData = list(exampleReader)
+
+labels = []
+values = []
+
+
+
 @app.route("/")
 def chart():
-    labels = ["January","February","March","April","May","June","July","August"]
-    values = [10,9,8,7,6,4,7,8]
-    return render_template('chart.html', values=values, labels=labels)
+	for row in exampleReader:
+		labels.append(row[0])
+		values.append(row[1])
+		values2 = [100, 250, 750]
+	return render_template('chart.html', set=zip(values, labels, values2))
  
 if __name__ == "__main__":
     app.run()
---
+++ Attempted Codes/Third trial/multiple_figs_demo.py
@@ -0,0 +1,10 @@
+import pylab as plt
+
+DayOfWeekOfCall = [1,2,3]
+DispatchesOnThisWeekday = [77, 32, 42]
+
+LABELS = ["Monday", "Tuesday", "Wednesday"]
+
+plt.bar(DayOfWeekOfCall, DispatchesOnThisWeekday, align='center')
+plt.xticks(DayOfWeekOfCall, LABELS)
+plt.show()
